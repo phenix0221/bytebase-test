8b1e1eefb3cbdfa0c7fce5c9b836b3871c85bc40
Yingqin He <heyingqin@cn.wilmar-intl.com>
2024-04-02 15:40:38 +0800

    1

--- bytebase/test/prod/user##202404021000##ddl##create_tableuser.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    birthdate DATE,
-    is_active BOOLEAN DEFAULT TRUE
-);
